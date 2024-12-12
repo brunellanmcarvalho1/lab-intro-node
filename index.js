d89ed7e2cd05ae18bb7690be2fad0df7e56c9d04
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2024-12-12 09:48:03 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let index = 0;
+    while (index < this.length && this.items[index] < item) {
+      index++;
+    }
+    this.items.splice(index, 0, item);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos > this.items.length) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    sum = this.items.reduce((num, currentValue) => {
+      return num + currentValue;
+    }, 0);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
